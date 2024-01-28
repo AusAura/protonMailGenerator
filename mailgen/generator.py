ed4abbac8c8838a9f7acdb122a2373e4df128d27
AusAura <91804331+AusAura@users.noreply.github.com>
2024-01-28 17:00:44 +0200

    BugFix

--- mailgen/generator.py
+++ mailgen/generator.py
@@ -10,14 +10,17 @@ import webbrowser
 import ctypes
 import re
 
-CF_TEXT = 1
+freeze_time_rate = 3
 
+## for clipboard
+CF_TEXT = 1
 kernel32 = ctypes.windll.kernel32
 kernel32.GlobalLock.argtypes = [ctypes.c_void_p]
 kernel32.GlobalLock.restype = ctypes.c_void_p
 kernel32.GlobalUnlock.argtypes = [ctypes.c_void_p]
 user32 = ctypes.windll.user32
 user32.GetClipboardData.restype = ctypes.c_void_p
+
 
 def getClip6digit():
     user32.OpenClipboard(0)
@@ -28,9 +31,12 @@ def getClip6digit():
             text = ctypes.c_char_p(data_locked)
             value = text.value
             kernel32.GlobalUnlock(data_locked)
-            return str(re.findall(r'(\d{6})', (str(value))))
+            print(data, data_locked)
+            print(text, value)
+            return re.search(r'(\d{6})', (str(value)))
     finally:
         user32.CloseClipboard()
+
 
 def getMail():
     user32.OpenClipboard(0)
@@ -41,25 +47,21 @@ def getMail():
             text = ctypes.c_char_p(data_locked)
             value = text.value
             kernel32.GlobalUnlock(data_locked)
-            if "@dropmail.me" in str(value)  or "@10mail.org"  in str(value)  or "@emlpro.com" in str(value) or "@emltmp.com" in str(value): # 
+            if "@yomail.info" in str(value)  or "@10mail.org"  in str(value)  or "@emlpro.com" in str(value) or "@emltmp.com" in str(value): # 
                 match = re.search(r'[\w.+-]+@[\w-]+\.[\w.-]+', str(value))
                 return str(match.group(0))
             return False
     finally:
         user32.CloseClipboard()
-webbrowser.open('https://google.com')
-
-time.sleep(5)
-pyautogui.keyDown('ctrlleft'); pyautogui.keyDown('shift'); pyautogui.typewrite('p'); pyautogui.keyUp('ctrlleft'); pyautogui.keyUp('shift')
-pyautogui.typewrite('https://account.proton.me/signup?plan=free\n')
-time.sleep(5)
-
 
 
 def randomize(
                 _option_,
                 _length_
             ):
+    """
+    Random passwords and mails generator
+    """
 
     if _length_ > 0 :
 
@@ -93,100 +95,137 @@ def randomize(
                 _generated_info_= _generated_info_ + random.choice(string._characters_)
 
         return _generated_info_
-
     else:
         return 'error'
 
-# Username
-_username_=randomize('-s',5)+randomize('-s',5)+randomize('-s',5)
-pyautogui.typewrite(_username_ + '\t\t')
-print("Username:" + _username_)
 
-# Password
-_password_=randomize('-p',16)
-pyautogui.typewrite(_password_+'\t'+_password_+'\t')
-print("Password:" + _password_)
+if __name__ == '__main__':
 
-pyautogui.typewrite('\n')
-time.sleep(5)
-pyautogui.typewrite('\t\t\t\n')
+    webbrowser.open('https://google.com') ## start browser
+    time.sleep(2 * freeze_time_rate)
 
-pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('t'); pyautogui.keyUp('ctrlleft')
+    # pyautogui.keyDown('ctrlleft') 
+    # pyautogui.keyDown('shift') 
+    # pyautogui.typewrite('p') 
+    # pyautogui.keyUp('ctrlleft') 
+    # pyautogui.keyUp('shift') # ctrl + shift + P for private
 
-time.sleep(10)
-pyautogui.typewrite('https://dropmail.me/\n')
+    pyautogui.keyDown('ctrlleft') 
+    pyautogui.typewrite('n') 
+    pyautogui.keyUp('ctrlleft') # ctrl + N for new tab
+
+    pyautogui.typewrite('https://account.proton.me/signup?plan=free\n') ## type, go to Proton
+    time.sleep(5 * freeze_time_rate)
+
+    # Making Username
+    _username_=randomize('-s',5)+randomize('-s',5)+randomize('-s',5)
+    pyautogui.typewrite(_username_ + '\t\t\t')
+    print("Username:" + _username_)
+
+    # Making Password
+    _password_=randomize('-p',16)
+    pyautogui.typewrite(_password_+'\t'+_password_+'\t')
+    print("Password:" + _password_)
+
+    pyautogui.typewrite('\n') ## enter
+    time.sleep(2 * freeze_time_rate)
+    pyautogui.typewrite('\t\t\t\n') # switch to email confirmation
+
+    pyautogui.keyDown('ctrlleft')  
+    pyautogui.typewrite('t') 
+    pyautogui.keyUp('ctrlleft') ## new tab
+
+    time.sleep(1 * freeze_time_rate)
+    pyautogui.typewrite('https://dropmail.me/en/\n') ## type, go to dropmail
 
 
-pyautogui.keyDown('shift');pyautogui.keyDown('down'); pyautogui.keyUp('down'); pyautogui.keyUp('shift')
-time.sleep(10)
+    pyautogui.keyDown('shift')
+    pyautogui.keyDown('down') 
+    pyautogui.keyUp('down') 
+    pyautogui.keyUp('shift') ## select the page instead of address bar
+    time.sleep(1 * freeze_time_rate)
 
-newMail = True
-while True:
-    if not newMail:
-        pyautogui.keyDown('ctrlleft'); pyautogui.typewrite('r'); pyautogui.keyUp('ctrlleft')
-        time.sleep(5)
-    pyautogui.typewrite('\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t')#28
+    newMail = True
+    while True:
+        if not newMail:
+            pyautogui.keyDown('ctrlleft')
+            pyautogui.typewrite('r')
+            pyautogui.keyUp('ctrlleft') ## refresh the page if mail is not there
+            time.sleep(1 * freeze_time_rate)
+
+        pyautogui.typewrite('\t' * 28)  ## select the email address element
+
+        pyautogui.keyDown('ctrlleft')
+        pyautogui.keyDown('shiftleft')
+        pyautogui.keyDown('shiftright')
+        pyautogui.press('down')
+        pyautogui.keyUp('shiftleft')
+        pyautogui.keyUp('shiftright')
+        pyautogui.keyUp('ctrlleft') ## select only the email address string
+
+
+        pyautogui.keyDown('ctrlleft') 
+        pyautogui.typewrite('c') 
+        pyautogui.keyUp('ctrlleft') ## copy
+
+        ## get the email from the clipboard to the script. Sometimes generated email domain is disabled in Proton
+        newMail = getMail() 
+
+        if newMail:
+            print("10 min mail: " + newMail)
+            break ## if success
+
     pyautogui.keyDown('ctrlleft')
-    pyautogui.keyDown('shiftleft')
-    pyautogui.keyDown('shiftright')
-    pyautogui.press('down')
-    pyautogui.keyUp('shiftleft')
-    pyautogui.keyUp('shiftright')
+    pyautogui.typewrite('\t') ## switch back to proton tab
     pyautogui.keyUp('ctrlleft')
-    pyautogui.keyDown('ctrlleft'); pyautogui.typewrite('c'); pyautogui.keyUp('ctrlleft')
-    newMail = getMail()
-    if newMail:
-        print("10 min mail: " + newMail)
-        break
+    time.sleep(1 * freeze_time_rate)
 
-pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('\t'); pyautogui.keyUp('ctrlleft')
-time.sleep(1)
-#äpyautogui.typewrite(newMail)
-pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('v'); pyautogui.keyUp('ctrlleft')
-pyautogui.press('backspace')
-pyautogui.typewrite('\n')
+    pyautogui.keyDown('ctrlleft');  
+    pyautogui.typewrite('v'); ## paste the email from the clipboard
+    pyautogui.keyUp('ctrlleft')
+    pyautogui.press('backspace') ## delete one additional space in the end
+    pyautogui.typewrite('\n') ## submit
 
-time.sleep(10)
+    time.sleep(10 * freeze_time_rate)
 
-pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('\t'); pyautogui.keyUp('ctrlleft')
-time.sleep(1)
+    pyautogui.keyDown('ctrlleft')
+    pyautogui.typewrite('\t')
+    pyautogui.keyUp('ctrlleft') ## switch to email confirmation
+    time.sleep(1 * freeze_time_rate)
 
-#pyautogui.typewrite('\t\t\t\t\t\t\t\t\t\t\t\t\t\n')
+    pyautogui.typewrite("\t" * 14 + "\n") ## select the 'HTML-design'
 
-#time.sleep(5)
+    pyautogui.keyDown('shiftleft')
+    pyautogui.typewrite('\t\t'); ## select the code window 
+    pyautogui.keyUp('shiftleft')
 
+    pyautogui.keyDown('ctrlleft'); 
+    pyautogui.typewrite('a'); ## select the text
+    pyautogui.keyUp('ctrlleft')
 
-pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('a'); pyautogui.keyUp('ctrlleft')
-pyautogui.keyDown('ctrlleft'); pyautogui.typewrite('c'); pyautogui.keyUp('ctrlleft')
+    pyautogui.keyDown('ctrlleft'); 
+    pyautogui.typewrite('c'); ## copy
+    pyautogui.keyUp('ctrlleft')
 
+    pyautogui.keyDown('ctrlleft')
+    pyautogui.typewrite('\t') ## to proton tab
+    pyautogui.keyUp('ctrlleft')
 
-pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('\t'); pyautogui.keyUp('ctrlleft')
-time.sleep(5)
-pyautogui.typewrite(str(getClip6digit()) + '\n')
+    time.sleep(1 * freeze_time_rate)
 
+    ver_code = getClip6digit().group()
+    print(ver_code)
+    pyautogui.typewrite(ver_code + '\n') ## extracts and types the code from the clipboard
 
-time.sleep(5)
-pyautogui.typewrite('\n')
-time.sleep(5)
-pyautogui.typewrite('\t\t\t\t\n')
-time.sleep(1)
-pyautogui.typewrite('\t\n')
+    time.sleep(5 * freeze_time_rate)
+    pyautogui.typewrite('\n') ## enter, selects the displayed username
 
-print(_username_+"@proton.me:" + _password_)
+    time.sleep(5 * freeze_time_rate)
+    pyautogui.typewrite('\t\t\t\n') ## skip the phone number assignation
 
-logfile = open("accLog.txt", "a")
-logfile.write(_username_ + "@proton.me:" + _password_ + "\n")
-logfile.close()
+    time.sleep(1 * freeze_time_rate)
+    pyautogui.typewrite('\t\n') ## confirm
 
-
-
-# CHAPTCHA
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-
-#pyautogui.typewrite('\n')
+    logfile = open("accountLog.txt", "a") ## saving details to the file
+    logfile.write(_username_ + "@proton.me:" + _password_ + "\n")
+    logfile.close()
